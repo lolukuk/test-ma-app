9769c455f37fd168637b0faac41d7827361783aa
lolukuk <demogs1@gmail.com>
2024-08-10 13:53:46 +0700

    refactor and add tests

--- tests/test_upload.py
+++ tests/test_upload.py
@@ -5,11 +5,13 @@ client = TestClient(app)
 
 
 def test_upload_file():
-    with open("tests/test_file.txt", "wb") as f:
+    with open("test_file.txt", "wb") as f:
         f.write(b"Test file content")
 
-    with open("tests/test_file.txt", "rb") as f:
+    with open("test_file.txt", "rb") as f:
         response = client.post("/upload/", files={"file": f})
 
     assert response.status_code == 200
     assert "uid" in response.json()
+
+# Так как апишки у меня нет, то прописал тесты для того, как они могут выглядеть, в локалке все работает
